fa67fd772f8cf74a332fc4974017ffd7e8e4d38e
Kyduckett <124756923+Kyduckett@users.noreply.github.com>
2023-02-11 22:36:59 -0600

    Switched to Shecodes Weather Api
    also deleted unused code from old homework.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,3 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
-//let citySearch = prompt("Enter a City");
-//citySearch = citySearch.toLowerCase();
-
-//if (weather[citySearch] !== undefined) {
-//let temperature = weather.[citySearch].temp
-//let celciusTemp = Math.round(temperature);
-//let fahrenheitTemp = (temperature * 9/5) + 32;
-//let humidity = weather.[citySearch].humidity
-
-//alert(`It is currently ${celciusTemp}°C (${fahrenheitTemp}) in ${citySearch} with a humidity of ${humidity}`);
-//}
-// else {
-//  alert("Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" +(citySearch)
-//);
-//}
-
 let now = new Date();
 let currentDateDisplay = document.querySelector("#current-day");
 function formateDate() {
@@ -77,10 +38,10 @@ function showWeather(response) {
 }
 
 function showPosition(position) {
-  let apiKey = "c93e97809431cb4a1503908d50079963";
+  let apiKey = "a90ac0f0b0448oc3f53c03t382594470";
   let long = position.coords.longitude;
   let lati = position.coords.latitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${long}&lat=${lati}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showWeather);
 }
 
